0bd3b5814410daf315590f3a813ff56d1edbfef0
Euluna Gotami <eulunag@gmail.com>
2026-03-05 23:35:05 -0800

    done

--- mybarrier.c
+++ mybarrier.c
@@ -1,13 +1,32 @@
 // mybarrier.c
 #include "mybarrier.h"
 
-void my_barrier_init(my_barrier_t *b, int n) {
+void my_barrier_init(my_barrier_t *b, int n)
+{
     spinlock_init(&b->slock);
     b->total_threads = n;
     b->count = 0;
     b->phaseNum = 0;
 }
 // This function is basically called when thread has done its phase and now wait for others to reach.
-void my_barrier_wait(my_barrier_t *b) {
-   // implement this.
+void my_barrier_wait(my_barrier_t *b)
+{
+    // implement this.
+    spinlock_acquire(&b->slock);
+    b->count++;
+    int local_phase = b->phaseNum;
+    if (b->count < b->total_threads)
+    {
+        spinlock_release(&b->slock);
+        while (b->phaseNum == local_phase)
+        {
+            sched_yield();
+        }
+    }
+    else if (b->count == b->total_threads)
+    {
+        b->count = 0;
+        b->phaseNum++;
+        spinlock_release(&b->slock);
+    }
 }
